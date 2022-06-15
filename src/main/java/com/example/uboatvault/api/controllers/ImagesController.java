f0930eef165a7c50bddfac339b522969a29e7625
Martoncă Daniel <45790572+danielmartonca@users.noreply.github.com>
2022-06-15 14:57:58 +0300

    Improved images controller logic

--- src/main/java/com/example/uboatvault/api/controllers/ImagesController.java
+++ src/main/java/com/example/uboatvault/api/controllers/ImagesController.java
@@ -1,7 +1,6 @@
 package com.example.uboatvault.api.controllers;
 
 import com.example.uboatvault.api.services.ImagesService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,30 +20,22 @@ public class ImagesController {
 
     @GetMapping(value = "/images/getDefaultProfilePicture", produces = MediaType.IMAGE_PNG_VALUE)
     public @ResponseBody
-    byte[] getDefaultProfilePicture() {
-        return imagesService.getDefaultProfilePicture();
+    ResponseEntity<byte[]> getDefaultProfilePicture() {
+        var bytes = imagesService.getDefaultProfilePicture();
+        return ResponseEntity.ok(bytes);
     }
 
     @GetMapping(value = "/images/getSailorProfilePicture", produces = MediaType.IMAGE_PNG_VALUE)
     public @ResponseBody
     ResponseEntity<byte[]> getSailorProfilePicture(@RequestParam(name = "sailorId") String sailorId) {
         var bytes = imagesService.getSailorProfilePicture(sailorId);
-
-        if (bytes == null)
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-
-
-        return new ResponseEntity<>(bytes, HttpStatus.OK);
+        return ResponseEntity.ok(bytes);
     }
 
     @GetMapping(value = "/images/getSailorBoatImages", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     ResponseEntity<List<byte[]>> getSailorBoatImages(@RequestParam(name = "sailorId") String sailorId) {
         var imagesBytesList = imagesService.getSailorBoatImages(sailorId);
-
-        if (imagesBytesList == null)
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-
-        return new ResponseEntity<>(imagesBytesList, HttpStatus.OK);
+        return ResponseEntity.ok(imagesBytesList);
     }
 }
