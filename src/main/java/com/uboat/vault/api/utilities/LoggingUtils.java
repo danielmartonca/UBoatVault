86e7e4199a45e0e1629498f63f4a9d08ae885d17
Martoncă Daniel <45790572+danielmartonca@users.noreply.github.com>
2022-09-16 17:41:21 +0300

    Added pretty printing option set in app props

--- src/main/java/com/uboat/vault/api/utilities/LoggingUtils.java
+++ src/main/java/com/uboat/vault/api/utilities/LoggingUtils.java
@@ -4,11 +4,22 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class LoggingUtils {
     private static final Logger log = LoggerFactory.getLogger(LoggingUtils.class);
+
+    @Value("${uboat.pretty-print-logs}")
+    private void setPrettyPrintLogs(String prettyPrintLogs) {
+        LoggingUtils.PRETTY_PRINT_LOGS = prettyPrintLogs;
+    }
+
+    private static String PRETTY_PRINT_LOGS;
 
     public static String colorString(String string, TextColor color) {
         return color.getColorCode() + string + TextColor.RESET.getColorCode();
@@ -38,6 +49,9 @@ public class LoggingUtils {
         if (api.contains("swagger") || api.contains("api-docs"))
             body = "";
 
+        if (PRETTY_PRINT_LOGS != null && PRETTY_PRINT_LOGS.equalsIgnoreCase("true"))
+            body = toStringFormatted(body);
+
         String suffix = "       REQUEST:";
         log.info(colorsBasedData(suffix, requestMethod, api, queryParams, body));
     }
@@ -45,6 +59,9 @@ public class LoggingUtils {
     public static void logResponse(HttpMethod requestMethod, String api, String queryParams, String body) {
         if (api.contains("swagger") || api.contains("api-docs"))
             body = "";
+
+        if (PRETTY_PRINT_LOGS != null && PRETTY_PRINT_LOGS.equalsIgnoreCase("true"))
+            body = toStringFormatted(body);
 
         String suffix = "       RESPONSE:";
         log.info(colorsBasedData(suffix, requestMethod, api, queryParams, body));
@@ -81,6 +98,17 @@ public class LoggingUtils {
         } catch (JsonProcessingException e) {
             log.error("Failed to print object of type {}.", object.getClass().getSimpleName(), e);
             return object.toString();
+        }
+    }
+
+    public static String toStringFormatted(String string) {
+        try {
+            if (string == null || string.isEmpty()) return string;
+            var jsonObject = new JSONObject(string);
+            return jsonObject.toString(4);
+        } catch (JSONException e) {
+            log.error("Failed to pretty print object of type {}.", String.class);
+            return string;
         }
     }
 }
