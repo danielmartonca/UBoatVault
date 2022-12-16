cd435bf1f0454b2c3a3a1edb9c4170c4bf7e3a92
Martoncă Daniel <45790572+danielmartonca@users.noreply.github.com>
2022-12-16 14:21:54 +0200

    Implemented locking for sailors and journies due to concurrency issues

--- src/main/java/com/uboat/vault/api/persistence/repostiories/JourneyRepository.java
+++ src/main/java/com/uboat/vault/api/persistence/repostiories/JourneyRepository.java
@@ -5,23 +5,30 @@ import com.uboat.vault.api.model.domain.account.sailor.Sailor;
 import com.uboat.vault.api.model.domain.sailing.Journey;
 import com.uboat.vault.api.model.enums.JourneyState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 public interface JourneyRepository extends JpaRepository<Journey, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Journey> findJourneysByState(JourneyState state);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Journey> findAllByClientAccount_IdAndState(Long clientAccountId, JourneyState state);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Journey> findAllByStateAndSailorAccount_Id(JourneyState state, Long sailorAccountId);
 
     void deleteAllByClientAccountAndStateIn(Account clientAccount, Set<JourneyState> state);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Journey> findBySailorAndState(Sailor sailor, JourneyState state);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT j from Journey  j where j.state=?1 and j.sailor.account.id=?2 and j.route.pickupLocation.coordinates.latitude=?3 and j.route.pickupLocation.coordinates.longitude=?4 and j.route.destinationLocation.coordinates.latitude=?5 and j.route.destinationLocation.coordinates.longitude=?6")
     Journey findJourneyOfSailorMatchingStatePickupAndDestination(JourneyState state, Long sailorId, double pickupLocationLatitude, double pickupLocationLongitude, double destinationLocationLatitude, double destinationLocationLongitude);
 }
--- src/main/java/com/uboat/vault/api/persistence/repostiories/SailorsRepository.java
+++ src/main/java/com/uboat/vault/api/persistence/repostiories/SailorsRepository.java
@@ -2,12 +2,19 @@ package com.uboat.vault.api.persistence.repostiories;
 
 import com.uboat.vault.api.model.domain.account.sailor.Sailor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 public interface SailorsRepository extends JpaRepository<Sailor, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Sailor> findAllByLookingForClients(boolean lookingForClients);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Sailor findSailorByIdAndLookingForClientsIsTrue(@Param("id") Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Sailor findFirstByAccountId(Long accountId);
 }
